16d6f1638570cf1fa6873ac42900387613cc3174
Derek Chavez <frrenose17@yahoo.com>
2024-10-10 10:01:49 -0600

    Setup Checly repo

---
+++ src/__checks__/homepage.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('webshop homepage', async ({ page, baseURL }) => {
+  const response = await page.goto(baseURL);
+
+  expect(response?.status()).toBeLessThan(400);
+  await expect(page).toHaveTitle(/Danube WebShop/);
+  await page.screenshot({ path: 'homepage.jpg' });
+});
